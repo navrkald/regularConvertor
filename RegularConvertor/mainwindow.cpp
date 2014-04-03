6519c328f4046e61b90fe13f4f316271b19e2e19
navrkald <navrkald@centrum.cz>
2014-04-03 11:31:29 +0200

    Comment clean.

--- RegularConvertor/mainwindow.cpp
+++ RegularConvertor/mainwindow.cpp
@@ -14,27 +14,11 @@ MainWindow::MainWindow(QWidget *parent) :
 {
 
     ui->setupUi(this);
-//TODO: dodelat tlacitka dovnitr
-//http://doc.qt.digia.com/qt/qgraphicsproxywidget.html#details
-//https://www.google.com/search?q=QGraphicsProxyWidget&aq=f&oq=QGraphicsProxyWidget&sourceid=chrome&ie=UTF-8#hl=cs&tbo=d&sclient=psy-ab&q=qgraphicsproxywidget+position+upper+right&oq=qgraphicsproxywidget+position+upper+right&gs_l=serp.3...15672.20351.0.20431.12.12.0.0.0.0.84.838.12.12.0...0.0...1c.1.UjcavTFbebs&pbx=1&bav=on.2,or.r_gc.r_pw.r_cp.r_qf.&fp=bf1270eeb39e6808&bpcl=40096503&biw=1738&bih=824
-//    sceneBut = new QToolButton;
-
-//    scene.addWidget(sceneBut);
-    //FiniteAutomata* FA_1 = new FiniteAutomata(this);
-    //FiniteAutomata* FA_2 = new FiniteAutomata(this);
-    //scene1 = new DiagramScene(FA_1, this);
-    //scene2 = new DiagramScene(FA_2, this);
-    //ui->graphicsView1->setScene(scene1);
-    //ui->graphicsView2->setScene(scene2);
-    //ui->graphicsView1->show();
-    //ui->graphicsView2->show();
 
     statusBarTimeout = 5000; //5 second
     setWindowTitle(MY_WINDOW_TITLE);
     connect(ui->action_RE_to_FA,SIGNAL(triggered()),this,SLOT(prepareREtoFA()));
 
-    //ui->action_RE_to_FA->triggered(true);
-    //ui->action_play_mode->triggered(true);
 
     QActionGroup* modesGroup = new QActionGroup(this);
     modesGroup->addAction(ui->action_check_mode);
@@ -52,6 +36,11 @@ MainWindow::MainWindow(QWidget *parent) :
     alhgorithm_widget = new AlgorithmWidget(mode,this);
     alhgorithm_widget->hide();
     connect(this, SIGNAL(modeChanged(Algorithm::modes)), alhgorithm_widget, SLOT(setWidgets(Algorithm::modes)));
+
+    //    deleteShortCut = new QShortcut(QKeySequence::Delete, this);
+    //    connect( deleteShortCut, SIGNAL(activated()), FA1_widget->scene, SLOT(deleteSelected()));
+    //    connect(FA1_widget,SIGNAL(errorMessageSignal(QString)),this, SLOT(myStatusbarShowMessage(QString)));
+    //    connect(FA1_widget->scene,SIGNAL(sendErrorMessage(QString)),this,SLOT(myStatusbarShowMessage(QString))
 }
 
 MainWindow::~MainWindow()
@@ -183,34 +172,6 @@ void MainWindow::prepareREtoFA(RegExp* _re)
     {
         ;
     }
-
-
-
-//    AlgorithmView* listView = new AlgorithmView(this);
-//    listView->setMouseTracking(true);
-
-
-//    QStandardItemModel * model = new QStandardItemModel(10,1);
-//    HTMLDelegate* delegate = new HTMLDelegate();
-//    listView->setModel(model);
-//    listView->setItemDelegate(delegate);
-//    model->insertRows(4,4,QModelIndex());
-//    for(int i = 0; i < 4; i++)
-//    {
-
-//        QModelIndex index = model->index(i,0,QModelIndex());
-//        model->setData(index,"a<sup>2</sup><sub style='position: relative; left: -.5em;'>i</sub?",Qt::DisplayRole);
-//        model->setData(index, Qt::Unchecked, Qt::CheckStateRole);
-
-//    }
-//
-
-//    deleteShortCut = new QShortcut(QKeySequence::Delete, this);
-//    connect( deleteShortCut, SIGNAL(activated()), FA1_widget->scene, SLOT(deleteSelected()));
-
-
-//    connect(FA1_widget,SIGNAL(errorMessageSignal(QString)),this, SLOT(myStatusbarShowMessage(QString)));
-//    connect(FA1_widget->scene,SIGNAL(sendErrorMessage(QString)),this,SLOT(myStatusbarShowMessage(QString)));
 
 }
 

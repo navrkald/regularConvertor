f5479c577fcb3db5bb27324c6ca20adff41cfcc3
David Navrkal <navrkald@gmail.com>
2017-04-16 16:40:39 +0200

    Dont allow to add arrow to PDA when PDA transition rules are empty.

--- src/PDA/pdadiagramscene.cpp
+++ src/PDA/pdadiagramscene.cpp
@@ -7,13 +7,15 @@ void CPDADiagramScene::AddArrow(StateNode *startItem, StateNode *endItem){
 	if(QDialog::Accepted == inputDialog.exec())
 	{
 		QSet<CPDACompotutationalRule> rules = inputDialog.GetPdaRules();
-		CPdaArrow* arrow = new CPdaArrow(startItem, endItem, m_pa, rules, 0, this);
-		m_pa->AddPdaRules(rules);
-		startItem->addArrow(arrow);
-		endItem->addArrow(arrow);
-		arrow->setZValue(-1000.0);   //posun na pozadi
-		addItem(arrow);
-		arrow->updatePosition();
-		// TODO: Emit FA changed // 						 emit FA_changed(FA);
+        if(!rules.isEmpty()){
+            CPdaArrow* arrow = new CPdaArrow(startItem, endItem, m_pa, rules, 0, this);
+            m_pa->AddPdaRules(rules);
+            startItem->addArrow(arrow);
+            endItem->addArrow(arrow);
+            arrow->setZValue(-1000.0);   //posun na pozadi
+            addItem(arrow);
+            arrow->updatePosition();
+            // TODO: Emit FA changed // 						 emit FA_changed(FA);
+        }
 	}
 }
